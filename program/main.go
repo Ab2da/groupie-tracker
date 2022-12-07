831f5338d58a660305638784595f2482537c1a31
Matthew Hope <matt@matthewhope.co.uk>
2022-12-07 19:16:53 +0000

    Scaffold architecture

--- program/main.go
+++ program/main.go
@@ -4,13 +4,5 @@ func setupServer() {
 	// http.HandleFunc("/", homePage)
 }
 
-type Artist struct {
-	name       string `json:"name"`
-	image      string `json:"image"`
-	year       int    `json:"year"`
-	firstAlbum int    `json:"firstAlbum"`
-	members    string `json:"members"`
-}
-
 func main() {
 }
