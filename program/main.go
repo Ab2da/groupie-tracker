5fa420ad9c876290108386a95ccf98f2ddab3903
Matthew Hope <matt@matthewhope.co.uk>
2022-12-11 10:42:43 +0000

    Print artists names/members

--- program/main.go
+++ program/main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
-	dal "dal/DAL"
+	"deedee/groupie-tracker/dal"
 	"fmt"
-
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Home Page! :D!")
+	log.Printf("Welcome to the Home Page! :D!")
 	fmt.Println("Endpoint Hit:homePage")
-	dal.GetArtistsData()
+	t, err := template.ParseFiles("./wwwroot/index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	t.Execute(w, "hi")
+	// dal.GetArtistsData()
 }
 
 func setupServer() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":1000", nil))
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("wwwroot"))
+	mux.Handle("/wwwroot/", http.StripPrefix("/wwwroot/", fs))
+	mux.HandleFunc("/", homePage)
+	err := http.ListenAndServe(":9999", mux)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
+// Right now, when you run the program, we are just
+// getting a list of artists with our DAL (data access layer)
+// functions, and printing out the name and members of each
 func main() {
-	setupServer()
-
+	// 	setupServer()
+	var artists []dal.ArtistDTM = dal.GetArtists()
+	for _, a := range artists {
+		fmt.Println(a.Name)
+		fmt.Printf("\nMembers:\n")
+		for _, m := range a.Members {
+			fmt.Println(m)
+		}
+		fmt.Println()
+	}
 }
