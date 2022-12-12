5d21b079f992a04880e56e23f4ebf1cb64207b1f
Matthew Hope <matt@matthewhope.co.uk>
2022-12-12 16:15:42 +0000

    Display Artists

--- program/main.go
+++ program/main.go
@@ -8,12 +8,14 @@ import (
 	"net/http"
 )
 
+var artists []dal.ArtistDTM
+
 type PageModel struct {
-	Title string
+	Artists []dal.ArtistDTM
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	var p PageModel = PageModel{Title: "Home"}
+	var p PageModel = PageModel{Artists: artists}
 	log.Printf("Welcome to the Home Page! :D!")
 	fmt.Println("Endpoint Hit:homePage")
 	t, err := template.ParseFiles("./wwwroot/MainLayout.html")
@@ -24,35 +26,38 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	// dal.GetArtistsData()
 }
 
-func events(w http.ResponseWriter, r *http.Request) {
-	var p PageModel = PageModel{Title: "Concerts"}
-	log.Printf("Welcome to the Home Page! :D!")
-	fmt.Println("Endpoint Hit:homePage")
-	t, err := template.ParseFiles("./wwwroot/MainLayout.html")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	t.Execute(w, p)
-	// dal.GetArtistsData()
-}
+// func events(w http.ResponseWriter, r *http.Request) {
+// 	var p PageModel = PageModel{Artists: artists}
+// 	log.Printf("Welcome to the Home Page! :D!")
+// 	fmt.Println("Endpoint Hit:homePage")
+// 	t, err := template.ParseFiles("./wwwroot/MainLayout.html")
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 	}
+// 	t.Execute(w, p)
+// 	// dal.GetArtistsData()
+// }
 
 func setupServer() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("wwwroot"))
 	mux.Handle("/wwwroot/", http.StripPrefix("/wwwroot/", fs))
 	mux.HandleFunc("/", homePage)
-	mux.HandleFunc("/events", events)
-	err := http.ListenAndServe(":9999", mux)
+	// mux.HandleFunc("/events", events)
+	err := http.ListenAndServe(":1234", mux)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+}
+
+func init() {
+	artists = dal.GetArtists()
 }
 
 // Right now, when you run the program, we are just
 // getting a list of artists with our DAL (data access layer)
 // functions, and printing out the name and members of each
 func main() {
-	var artists []dal.ArtistDTM = dal.GetArtists()
 	for _, a := range artists {
 		fmt.Println(a.Name)
 		fmt.Printf("\nMembers:\n")
