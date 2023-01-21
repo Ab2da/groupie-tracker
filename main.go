81fa00cd0bfab7aa6a22c2e1985ebec4b1622569
Matthew Hope <matt@matthewhope.co.uk>
2023-01-21 13:11:41 +0000

    Create route map for speed

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"deedee/groupie-tracker/dal"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 )
+
+const port string = ":1234"
 
 var artists []dal.ArtistDTM
 var relations []dal.RelationDTM
+var artistRouteMap map[string]bool
+var artistPathModelMap map[string]ArtistVM
+var artistModels []ArtistVM
 
 // var dates []dal.DateDTM
 // structs for the display model
@@ -29,7 +34,7 @@ type Event struct {
 	Dates    []string
 }
 
-type PageModel struct {
+type HomeModel struct {
 	Artists []ArtistVM
 }
 
@@ -59,20 +64,27 @@ func BuildArtistVM(a dal.ArtistDTM) ArtistVM {
 	return cardVM
 }
 
-func BuildAllArtistVMs(dtms []dal.ArtistDTM) []ArtistVM {
-	var result []ArtistVM
-	for _, a := range dtms {
-		artist := BuildArtistVM(a)
-		result = append(result, artist)
+func InitArtistPathModelMap(dtms []dal.ArtistDTM) {
+	artistPathModelMap = make(map[string]ArtistVM)
+	for _, artist := range dtms {
+		var model ArtistVM = BuildArtistVM(artist)
+		artistModels = append(artistModels, model)
+		artistPathModelMap[artist.Name] = model
 	}
-	return result
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+	// Verify HTTP method
+	if r.Method != http.MethodGet {
+		log.Printf("%s - %s - %d\n", r.Method, r.URL.Path, http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("This site does not support non-GET HTTP requests.\n"))
+		return
+	}
+	// Basic Routing
 	if r.URL.Path == "/" {
-		var p PageModel = PageModel{Artists: BuildAllArtistVMs(artists)}
-		log.Printf("Welcome to the Home Page! :D!")
-		fmt.Println("Endpoint Hit:homePage")
+		var p HomeModel = HomeModel{Artists: artistModels}
+		log.Printf("%s - %s - %d\n", r.Method, r.URL.Path, http.StatusOK)
 		t, err := template.ParseFiles("./wwwroot/MainLayout.html")
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -81,46 +93,52 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, p)
 		return
 	}
-
-	for _, v := range artists {
-		if r.URL.Path == "/"+v.Name {
-			var info ArtistVM = BuildArtistVM(v)
-			t, err := template.ParseFiles("./wwwroot/artists.html")
-			if err != nil {
-				log.Println(err.Error())
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			t.Execute(w, info)
+	// Routing - Artist Name
+	var name string = strings.TrimPrefix(r.URL.Path, "/")
+	if artistRouteMap[name] {
+		var model ArtistVM = artistPathModelMap[name]
+		t, err := template.ParseFiles("./wwwroot/artists.html")
+		if err != nil {
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		err = t.Execute(w, model)
+		if err != nil {
+			log.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		log.Printf("%s - %s - %d\n", r.Method, r.URL.Path, http.StatusOK)
+		return
 	}
+	// Print a 404 not found if page does not exist
+	log.Printf("%s - %s - %d\n", r.Method, r.URL.Path, http.StatusNotFound)
 	http.NotFound(w, r)
 }
 
-// Add defer close
 func setupServer() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("wwwroot"))
 	mux.Handle("/wwwroot/", http.StripPrefix("/wwwroot/", fs))
 	mux.HandleFunc("/", homePage)
-	err := http.ListenAndServe(":9876", mux)
+	err := http.ListenAndServe(port, mux)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	log.Printf("Server listening on port %s...\n", port)
 }
 
 func init() {
 	artists = dal.GetArtists()
 	relations = dal.GetRelations(artists)
+	artistRouteMap = make(map[string]bool)
+	for _, v := range artists {
+		artistRouteMap[v.Name] = true
+	}
+	InitArtistPathModelMap(artists)
 }
 
 func main() {
-	r := dal.GetRelations(artists)
-	for _, v := range r {
-		fmt.Println(v)
-	}
-
 	setupServer()
 }
 
